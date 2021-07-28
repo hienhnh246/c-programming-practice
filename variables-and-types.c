5ab02202bb076b1129bf4b8159324e8465562f68
hienhnh246 <hien.hnh246@gmail.com>
2021-07-28 16:50:32 +0700

    Create Arrays

--- variables-and-types.c
+++ variables-and-types.c
@@ -4,8 +4,11 @@ int main(){
 	int a = 3;
 	float b = 4.5;
 	double c = 5.25;
-	float sum = a + b + c;
+	float sum;
+
+	sum = a +b + c;
 
 	printf("The sum of a, b, and c is %f.", sum);
+
 	return 0;
 }
