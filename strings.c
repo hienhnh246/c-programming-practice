f2a27efa1a32eea995e11c8497e39782d58f9373
hienhnh246 <hien.hnh246@gmail.com>
2021-07-29 16:09:06 +0700

    Create Strings

---
+++ strings.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char *first_name = "John";
+	char last_name[] = "Doe";
+	char name[100];
+
+	last_name[0] = 'B';
+	sprintf(name, "%s %s", first_name, last_name);
+
+	if (strncmp(name, "John Boe", 100) == 0)
+		printf("Done!\n");
+
+	name[0] = '\0';
+	strncat(name, first_name, 4);
+	strncat(name, last_name, 20);
+	printf("%s\n", name);
+
+	char dest[20] = "Hello";
+	char src[20] = "World";
+	strncat(dest, src, 3);
+	printf("%s\n", dest);
+	strncat(dest, src, 20);
+	printf("%s\n", dest);
+
+	return 0;
+}
